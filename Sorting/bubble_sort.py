757160116d2756d13f86b09a75f2c6686ab45956
kishore-reddy-gayam <kishorereddycse860@gmail.com>
2026-07-16 11:15:33 +0530

    Add bubble sort implementation in bubble_sort.py
    Implemented bubble sort algorithm to sort integers.

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,13 @@
+# Bubble Sort - takes space-separated integers as input
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+
+nums = list(map(int, input().split()))
+a = len(nums)
+for i in range(a):
+    for j in range(a - 1):
+        if nums[j] > nums[j + 1]:
+            temp = nums[j]
+            nums[j] = nums[j + 1]
+            nums[j + 1] = temp
+print(nums
